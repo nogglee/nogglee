0b96693c748b1a9988979b4095b312b2bf6b0f8c
이은지 <dev.nogglee@gmail.com>
2025-05-14 23:54:43 +0900

    design: overview section 추가

--- web.mjs
+++ web.mjs
@@ -18,14 +18,16 @@ export async function Start()
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const TEMPLATE_DATA = [
 	{
-		title: '웨딩 플래너',
-		description: '결혼 준비를 위한 노션 템플릿입니다.',
+		id: 1,
+		title: '플래너',
+		description: '시간 관리를 위한 노션 템플릿입니다.',
 		type: 'notion',
 		link: '#',
 		video: 'https://www.youtube.com/embed/vmL-XgxwQZU?si=gcctn4yt752_3s0mm',
 		image: '/thumbnail.png'
 	},
 	{
+		id: 2,
 		title: '가계부 시트',
 		description: '구글시트 기반 가계부 템플릿입니다.',
 		type: 'sheets',
@@ -34,6 +36,7 @@ const TEMPLATE_DATA = [
 		image: '/thumbnail.png'
 	},
 	{
+		id: 3,
 		title: '일정 관리 보드',
 		description: '프로젝트 일정을 관리하는 노션 보드입니다.',
 		type: 'notion',
